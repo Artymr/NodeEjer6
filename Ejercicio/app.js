7c800bc6c4eeb552d714e21836afced3c2a50a2d
Arty <arturo.munozrodriguez@usp.ceu.es>
2025-11-25 11:34:15 +0100

    ya puedo conectar usuarios y sale el chat, pero mal

--- Ejercicio/app.js
+++ Ejercicio/app.js
@@ -3,56 +3,58 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require('express-session'); //nuevo
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-//para el chat
-var chatRouter = require('./routes/chat');
-
-
-//LO NUEVO DEL LOGIN VVVVVVVVV
 var loginRouter = require('./routes/login');
 var restringidaRouter = require('./routes/restringida');
+var chatRouter = require('./routes/chat');
 
-var app = express();//nuevo
+var app = express();
 
-app.use(session({  //nuevo
-  secret: 'claveParaCookies',
-  resave: false,
-  saveUninitialized: true
-}));
+var session = require("express-session");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// MIDDLEWARES
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// SESIÓN
+const sessionMiddleware = session({
+    secret: "claveParaCookies",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 600000 }
+});
+app.use(sessionMiddleware);
+app.set("sessionMiddleware", sessionMiddleware);
+
+// ESTÁTICOS
 app.use(express.static(path.join(__dirname, 'public')));
 
+// RUTAS NORMALES
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-//LO NUEVO DEL LOGIN VVVVVVVVV
 app.use('/login', loginRouter);
+
+// RUTAS PROTEGIDAS
 app.use('/restringida', restringidaRouter);
 app.use('/chat', chatRouter);
 
-// catch 404 and forward to error handler
+// 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// HANDLER ERRORES
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
